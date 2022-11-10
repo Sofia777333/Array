5ac42df63fdbc5176269196a28d7b1b80230e9ce
Sofia <117852305+Sofia777333@users.noreply.github.com>
2022-11-10 12:05:19 +0600

    Add files via upload

---
+++ Array.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace Array
+{
+     class Array
+    {
+        public int Length => _array.Length;
+        private int[] _array;
+        public Array(int size) => _array = new int[size];
+        public Array(params int[] array) => _array = array;
+        public int this [int index]
+        {
+            get => _array [index];
+            set => _array [index] = value;
+        }
+        public static Array operator +(Array lhs, Array rhs) 
+            => CalculateElements(lhs, rhs, (x, y) => x + y);
+        public static Array operator -(Array lhs, Array rhs)
+            => CalculateElements(lhs, rhs, (x, y) => x - y);
+
+        public static Array operator *(Array lhs, Array rhs)
+            => CalculateElements(lhs, rhs, (x, y) => x * y);
+
+
+        public static Array operator /(Array lhs, Array rhs)
+            => CalculateElements(lhs, rhs, (x, y) => x /y);
+
+        public static Array operator %(Array lhs, Array rhs)
+            => CalculateElements(lhs, rhs, (x, y) => x % y);
+        
+        public static Array CalculateElements(Array lhs, Array rhs, Func<int,int, int> operation)
+        
+        { Array min = lhs.Length > rhs.Length ? rhs : lhs;
+            Array max = ReferenceEquals(min, lhs) ? lhs : rhs;
+
+            Array array = new Array(max.Length);
+            for (int i = 0; i < max.Length; i++)
+            {
+                if (i >= min.Length)
+                {
+                    array[i] = max[i];
+                    continue;
+                }
+
+                array[i] = operation.Invoke(lhs[i], rhs[i]);
+            }
+            return array;
+        }
+           public void Print()
+           {
+            foreach (int element in _array)
+                Console.WriteLine($"{element}");
+
+           }
+
+    }
+     
+
+        
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Array
+{
+    class Program
+    {
+     static void Main()
+        {
+            Array arr1 = new Array(3, 5, 7, 9);
+            Array arr2 = new Array(2, 4, 6, 8);
+            (arr1 * arr2).Print();
+        }
+    }
+}
